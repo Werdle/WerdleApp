a0af6441b2b1bfe8dc91e5a093d98dfe88cbf609
kwang38 <47671423+kwang38@users.noreply.github.com>
2022-04-18 17:05:31 -0400

    Added world tiles to storyboard

--- Wordle/HomePageViewController.swift
+++ Wordle/HomePageViewController.swift
@@ -12,9 +12,9 @@ class HomepageViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+
     }
-    
+
 
     /*
     // MARK: - Navigation
